9b86d432e697722a0dc2045c30e3b0f196096da5
enzo <enzopaolo25@gmail.com>
2026-01-06 23:55:13 -0300

    changes

--- main.py
+++ main.py
@@ -1,1 +1,10 @@
-print("esta es la función main!")
+import ast, astor
+
+def main():
+    with open("test_1.py","r") as f:
+        file = f.read()
+        tree = ast.parse(file)
+        print(astor.dump_tree(tree))
+
+if __name__ == "__main__":
+    main()
